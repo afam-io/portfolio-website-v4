1305ccda293f58b85b49807a2c0dd45b911571f6
AfamOkoh <afamokoh@gmail.com>
2024-08-26 22:51:25 +0100

    feat: add card text icons to project cards

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -16,11 +16,28 @@ const RecentProjects = () => {
             key={id}
             className='lg:min-h-[32.5rem] h-[30rem] flex items-center justify-center  w-[80vw]'>
             <PinContainer title={title} href={link}>
-              <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh]'>
-                <div>
+              <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10'>
+                <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#131162d]'>
                   <img src='/bg.png' alt='bg-img' />
                 </div>
                 <img src={img} alt={title} className='z-10 absolute bottom-0' />
+              </div>
+              <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
+                {title}
+              </h1>
+              <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
+                {des}
+              </p>
+              <div className='flex items-center justify-between mt-7 mb-3'>
+                <div className='flex items-center'>
+                  {iconLists.map((icon) => (
+                    <div
+                      key={icon}
+                      className='border rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 border-white/[0.2]'>
+                      <img src={icon} alt={icon} className='p-2' />
+                    </div>
+                  ))}
+                </div>
               </div>
             </PinContainer>
           </div>
@@ -29,5 +46,4 @@ const RecentProjects = () => {
     </div>
   );
 };
-//  sm:w-96 w-[80-vw]
 export default RecentProjects;
