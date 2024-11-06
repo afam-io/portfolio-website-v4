b8da7e2ca443f99d081ff4cef1e4cf255995d386
AfamOkoh <afamokoh@gmail.com>
2024-11-06 12:45:41 +0000

    style: infinite moving cards

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -10,11 +10,13 @@ const Clients = () => {
         <span className='text-purple'> satisfied clients</span>
       </h1>
       <div className='flex flex-col items-center'>
-        <InfiniteMovingCards
-          items={testimonials}
-          direction='right'
-          speed='slow'
-        />
+        <div className='h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden'>
+          <InfiniteMovingCards
+            items={testimonials}
+            direction='right'
+            speed='slow'
+          />
+        </div>
       </div>
     </div>
   );
