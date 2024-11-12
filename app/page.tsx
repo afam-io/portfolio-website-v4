0beeeb165bbdf51395b85ba25dcf8cb5ec4cc940
AfamOkoh <afamokoh@gmail.com>
2024-11-12 15:28:22 +0000

    fix: comment out unused pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Clients from '@/components/Clients';
+// import Clients from '@/components/Clients';
 import Experience from '@/components/Experience';
 import Footer from '@/components/Footer';
 import Grid from '@/components/Grid';
 import Hero from '@/components/Hero';
 import Process from '@/components/Process';
-import RecentProjects from '@/components/RecentProjects';
+// import RecentProjects from '@/components/RecentProjects';
 import { FloatingNav } from '@/components/ui/FloatingNav';
 import { navItems } from '@/data';
 import Image from 'next/image';
