7566c2b4565641614793e17c02677dfe40199aae
AfamOkoh <afamokoh@gmail.com>
2025-03-05 16:30:35 +0000

    fix: set fixed duration for animation in Experience component

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -15,7 +15,7 @@ const Experience = () => {
             key={card.id}
             //borderRadius='1 rem'
             className='flex-1 text-white border-neutral-200 dark:border-slate-800'
-            duration={Math.floor(Math.random() * 10000) + 10000}>
+            duration={10000}>
             <div className='flex lg:flex-row lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2 flex-col'>
               <img
                 src={card.thumbnail}
