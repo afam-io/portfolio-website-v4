db6fdba5f30f64231578ac191a8f09a068126031
AfamOkoh <afamokoh@gmail.com>
2025-03-10 18:02:09 +0000

    feat: add semantic HTML elements

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -7,26 +7,26 @@ import { FaLocationArrow } from 'react-icons/fa';
 
 const RecentProjects = () => {
   return (
-    <div className='py-20' id='projects'>
-      <h1 className='heading'>
+    <section className='py-20' id='projects'>
+      <h2 className='heading'>
         A small selection of{' '}
         <span className='text-purple'> recent projects</span>
-      </h1>
+      </h2>
       <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
         {projects.map(({ id, title, des, img, iconLists, link }) => (
-          <div
+          <article
             key={id}
-            className='lg:min-h-[32.5rem] flex sm:h-[41rem]  h-[32rem] items-center justify-center  sm:w-[570px] w-[80vw]'>
+            className='lg:min-h-[32.5rem] flex sm:h-[41rem] h-[32rem] items-center justify-center sm:w-[570px] w-[80vw]'>
             <PinContainer title={title} href={link}>
-              <div className='relative sm:w-[570px] flex items-center justify-center sm:h-[40vh] h-[30vh]  w-[80vw] overflow-hidden  mb-10'>
+              <div className='relative sm:w-[570px] flex items-center justify-center sm:h-[40vh] h-[30vh] w-[80vw] overflow-hidden mb-10'>
                 <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#080A24]'>
                   <img src='/bg.png' alt='bg-img' />
                 </div>
                 <img src={img} alt={title} className='z-10 absolute bottom-0' />
               </div>
-              <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
+              <h3 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
                 {title}
-              </h1>
+              </h3>
               <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
                 {des}
               </p>
@@ -42,7 +42,7 @@ const RecentProjects = () => {
                   ))}
                 </div>
                 <div className='flex justify-center items-center'>
-                  <a href={link} className=''>
+                  <a href={link}>
                     <p className='flex lg:text-xl md:text-xs text-sm text-purple'>
                       {link.includes('github')
                         ? 'Check out the github repo'
@@ -53,10 +53,10 @@ const RecentProjects = () => {
                 </div>
               </div>
             </PinContainer>
-          </div>
+          </article>
         ))}
       </div>
-    </div>
+    </section>
   );
 };
 export default RecentProjects;
